4cd1fb38ed61d214210404456d72d8a7afe2cbbe
MezencevAS <36898922+MezencevAS@users.noreply.github.com>
2019-03-06 18:52:44 +0600

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+setup(
+    author="Levent Duivel",
+    include_package_data=True,
+    packages=find_packages(),
+)
