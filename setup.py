20cd7d59018a0237dab9b28198d1825e9d2ef47c
MezencevAS <36898922+MezencevAS@users.noreply.github.com>
2019-03-07 04:52:09 +0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,8 +3,17 @@
 
 from setuptools import setup, find_packages
 
+requirements = ['six', 'requests>=2.15,<3', 'parse>=1.8,<2', 'python-dateutil>=2.7,<3']
+
+setup_requirements = ['pytest-runner', 'six', 'requests>=2.15,<3', 'parse>=1.8,<2', 'python-dateutil>=2.7,<3']
+
+test_requirements = ['pytest', 'six', 'requests>=2.15,<3', 'parse>=1.8,<2', 'python-dateutil>=2.7,<3']
+
 setup(
     author="Levent Duivel",
     include_package_data=True,
     packages=find_packages(),
+    install_requires=requirements,
+    setup_requires=setup_requirements,
+    tests_require=test_requirements,
 )
